36412e07856d7f9e246c0fb0b0a8af6f8bda2b36
Julie Moon <8juliemoon@gmail.com>
2018-03-03 16:59:56 -0500

    add gulp and nav styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function(){
-  gulp.src('style.css')
+  gulp.src('*.css')
   .pipe(autoprefixer())
   .pipe(gulp.dest('build'))
 });
 
 gulp.task('watch',function(){
-  gulp.watch('style.css', gulp.series('styles'))
+  gulp.watch('*.css', gulp.parallel('styles'))
 });
